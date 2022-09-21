7b631d674cd82dca00d8af673b9e1081609e2f9b
kaylaberlanga <kaylaberlanga25@gmail.com>
2022-09-20 21:05:22 -0500

    new test

--- kayla.py
+++ kayla.py
@@ -6,3 +6,12 @@ with open(r'backup.txt', 'r') as fp:
         if any(words in line for words in words):
             count += 1 
 print('Total requests for Oct, 24, 1994 is', count)
+
+words = ["25/Oct/1994"]
+count =0
+with open(r'backup.txt', 'r') as fp:
+    lines = fp.readlines()
+    for line in lines:
+        if any(words in line for words in words):
+            count += 1 
+print('Total requests for Oct, 24, 1994 is', count)
