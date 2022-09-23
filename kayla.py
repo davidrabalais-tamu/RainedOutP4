a77e2735dafb649f8bac6f73a21582e4b026cde7
kaylaberlanga <kaylaberlanga25@gmail.com>
2022-09-22 23:50:44 -0500

    new test

--- kayla.py
+++ kayla.py
@@ -1,17 +1,22 @@
-words = ["24/Oct/1994"]
-count =0
-with open(r'backup.txt', 'r') as fp:
-    lines = fp.readlines()
-    for line in lines:
-        if any(words in line for words in words):
-            count += 1 
-print('Total requests for Oct, 24, 1994 is', count)
+FILE_NAME = './log.txt'
 
-words = ["25/Oct/1994"]
-count =0
-with open(r'backup.txt', 'r') as fp:
-    lines = fp.readlines()
-    for line in lines:
-        if any(words in line for words in words):
-            count += 1 
-print('Total requests for Oct, 24, 1994 is', count)
+# Use open() to get a filehandle that can access the file
+fh = open(FILE_NAME)
+
+#Katherine updated array dates
+mon_array = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+y_array = ["1994","1995"]
+day_array = ["01", "02","03", "04", "05", "06", "07", "08", "09","10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
+
+# Alternately, skip the assignment to the filehandle altogether:
+for year in y_array: 
+    for months in mon_array:
+        for days in day_array:
+            x=0
+            for line in open(FILE_NAME):
+                if ("GET" in line) and (days+"/"+months+"/"+year+":" in line):
+                    x=x+1
+            print ("There were ", x, "requests on", days, months, year)
+
+
+fh.close()      # close the file when you're finished with it
