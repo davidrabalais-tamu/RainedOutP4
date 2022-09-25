3cdca771371d08a804f5a77c621807005c5eaa88
kaylaberlanga <113000241+kaylaberlanga@users.noreply.github.com>
2022-09-24 23:34:08 -0500

    Update kayla.py

--- kayla.py
+++ kayla.py
@@ -1,20 +1,23 @@
-import date
-d = {}
-requests=0
+from datetime import datetime
 
-with open("backup.txt") as fh:
-    Lines = fh.readlines()
-for line in Lines:
-        requests=+1
+FILE_NAME = 'log.txt'
 
-date = date.split()
-date=date 
-if date[0] in d:
-    d[date[0]] += 1
-else:
-    d[date[0]] = 1
+# Use open() to get a filehandle that can access the file
+fh = open(FILE_NAME)
 
-print("\n")
-print("Requests per day: ")
+# Katherine updated array dates
+mon_array = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+y_array = ["1994","1995"]
+day_array = ["01", "02","03", "04", "05", "06", "07", "08", "09","10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
 
-fh.close()
+# Alternately, skip the assignment to the filehandle altogether:
+for year in y_array: 
+     for months in mon_array:
+         for days in day_array:
+             requs=0
+             for line in open(FILE_NAME):
+                 if ("GET" in line) and (days+"/"+months+"/"+year+":" in line):
+                     requs=requs+1
+             print ("There were ", requs, "requests on", days, months, year)
+
+fh.close()      # close the file when you're finished with it
