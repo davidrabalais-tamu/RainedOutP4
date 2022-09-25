6af1d0e9eb01e5e6bfac74c0e983c197bf572b1f
kaylaberlanga <kaylaberlanga25@gmail.com>
2022-09-24 23:47:09 -0500

    new test

--- kayla.py
+++ kayla.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-<<<<<<< HEAD
 
 FILE_NAME = './log.txt'
 
@@ -80,29 +79,3 @@ fh.close()      # close the file when you're finished with it
 #   else:
 #     # This is a new filename -- let's add it to the dictionary
 #     things[filename] = 1
-
-
-=======
-
-FILE_NAME = 'log.txt'
-
-# Use open() to get a filehandle that can access the file
-fh = open(FILE_NAME)
-
-# Katherine updated array dates
-mon_array = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-y_array = ["1994","1995"]
-day_array = ["01", "02","03", "04", "05", "06", "07", "08", "09","10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
-
-# Alternately, skip the assignment to the filehandle altogether:
-for year in y_array: 
-     for months in mon_array:
-         for days in day_array:
-             requs=0
-             for line in open(FILE_NAME):
-                 if ("GET" in line) and (days+"/"+months+"/"+year+":" in line):
-                     requs=requs+1
-             print ("There were ", requs, "requests on", days, months, year)
-
-fh.close()      # close the file when you're finished with it
->>>>>>> 3cdca771371d08a804f5a77c621807005c5eaa88
