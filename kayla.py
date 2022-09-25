b6b2d300aa30bed1441d724c6f0ce396630081e3
kaylaberlanga <kaylaberlanga25@gmail.com>
2022-09-24 20:44:26 -0500

    new test

--- kayla.py
+++ kayla.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 FILE_NAME = './log.txt'
 
 # Use open() to get a filehandle that can access the file
@@ -12,11 +14,11 @@ day_array = ["01", "02","03", "04", "05", "06", "07", "08", "09","10", "11", "12
 for year in y_array: 
     for months in mon_array:
         for days in day_array:
-            x=0
+            requs=0
             for line in open(FILE_NAME):
                 if ("GET" in line) and (days+"/"+months+"/"+year+":" in line):
-                    x=x+1
+                    requs=requs+1
             #if x > 0:       
-            print ("There were ", x, "requests on", days, months, year)
+            print ("There were ", requs, "requests on", days, months, year)
 
 fh.close()      # close the file when you're finished with it
