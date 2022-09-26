8b40767d2d408ea0604d3487435de298ee10e395
davidrabalais-tamu <davidrabalais@tamu.edu>
2022-09-25 21:45:28 -0500

    commiting final changes to davidparsing.py

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-print ('This is the program for Group 6 Project #3')
+print ('This is the program for Group 6 Project #4')
 print('\nTo run the program for this project you will need to first perform the following steps:\n1. sudo apt update\n2. sudo apt upgrade\n3. sudo apt install python3-pip\n4. pip3 install tabulate\n')
 input('Press ENTER to continue or CTRL+C to quit...')
 print ('Downloading the log file...')
