cc8709dd618d7ff7b4b36414799e321121bc1f0b
chase <chasegreer@tamu.edu>
2022-09-25 21:40:17 -0500

    chase

--- chase parsing.py
+++ chase parsing.py
@@ -1,17 +1,25 @@
 FILE_NAME = './log.txt'
 import collections
+def leastcommon():
+    fh = open(FILE_NAME)
+    files = {}
+    for line in open(FILE_NAME):
+        if ("HTTP" in line) and ("GET" in line) and ("1994" in line or "1995" in line):
+            #print(line)
+            # Extract the date from the request line
+            filename = line[(line.index("T")+1):(line.index("H"))]
 
-logfile = open("log.txt", "r")
-clean_log=[]
+            #print(filename)
+            if filename in files:
+                files[filename] += 1
+            else:
+                files[filename] = 1
+        
+            
+    fh.close()      # close the file when you're finished with it
+    # for filename in files:
+    #     print(str (filename) + str(files))
+    print (collections.Counter(files.values()).most_common()[-1][0])
 
-for line in logfile:
-    try:
-        clean_log.append(line[line.index("GET")+4:line.index("")])
-    except:
-        pass
 
-counter = collections.Counter(clean_log)
-
-#Top requested URL
-for count in counter.most_common(1):
-    print(str(count[1]) + "	" + str(count[0]))
+leastcommon()
