faba1abfb574d6526ec94db4a2d7123e2031289a
davidrabalais-tamu <davidrabalais@tamu.edu>
2022-09-25 22:33:04 -0500

    made some final changes to chases parsing.py. Now it creates leastrequested.txt where all of the files that were only requested one time are stored

--- chase parsing.py
+++ chase parsing.py
@@ -7,7 +7,7 @@ def leastcommon():
         if ("HTTP" in line) and ("GET" in line) and ("1994" in line or "1995" in line):
             #print(line)
             # Extract the date from the request line
-            filename = line[(line.index("T")+1):(line.index("H"))]
+            filename = line[(line.index("GET")+4):(line.index("HTTP")-1)]
 
             #print(filename)
             if filename in files:
@@ -17,9 +17,16 @@ def leastcommon():
         
             
     fh.close()      # close the file when you're finished with it
+
     # for filename in files:
     #     print(str (filename) + str(files))
-    print (collections.Counter(files.values()).most_common()[-1][0])
+    temp = min(files.values())
+    least = [file for file in files if files[file] == temp]
+    least_requested = open("leastrequested.txt", "w")
+    #print("The least common files are: " + str(least))
+    least_requested.write("The least common files are: " + str(least))
+    least_requested.close()
+    # print (collections.Counter(files.values()).most_common()[-1][0])
 
 
 leastcommon()
