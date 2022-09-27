3d9ee3241a2d79d1bf6b5917d5a99ee8b5843a54
davidrabalais-tamu <davidrabalais@tamu.edu>
2022-09-27 15:21:22 -0500

    some last minute changes

--- program.py
+++ program.py
@@ -1,10 +1,10 @@
 print ('This is the program for Group 6 Project #4')
-print('\nTo run the program for this project you will need to first perform the following steps:\n1. sudo apt update\n2. sudo apt upgrade\n3. sudo apt install python3-pip\n4. pip3 install tabulate\n')
+print('\nTo run the program for this project you will need to first perform the following steps:\n1. sudo apt update\n2. sudo apt upgrade\n')
 input('Press ENTER to continue or CTRL+C to quit...')
 
 # Download the log file.
 print ('Downloading the log file...')
-#import download_log
+import download_log
 
 # How many requests were made on each day? 
 print ('\n \nKayla: Number of requests made each day.')
